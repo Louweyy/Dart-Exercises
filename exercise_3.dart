80ac14469d4d8995e3693709d58f25ef37d42a2d
Louie <arangorinjustinlouie@gmail.com>
2025-06-16 13:57:43 +0800

    Add exercise_3.dart

---
+++ exercise_3.dart
@@ -0,0 +1,9 @@
+void main(){
+  //Exercise 3: List (print out elements of the list that are less than 35)
+
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+  for(int i = 0; a[i] < 35; i++ ){
+    print(a[i]);
+  }
+}
